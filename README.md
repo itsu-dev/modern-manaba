422ddf12d5275af74185df2ce78c389062408ae7
itsu-dev <itsu020402@gmail.com>
2022-01-11 20:57:37 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+[![GPL License](http://img.shields.io/badge/license-GPL-blue.svg?style=flat)](LICENSE)
+
+---
+
+<h3 align="center" > modern-manaba (beta) </h3>
+<p align="center">
+:star:
+</p>
+<p align="center"> 
+<sup>
+<b> ManabaのUIを変えるChrome拡張 </b>
+</sup>
+</p>
+
+--- 
+![](screenshot.png)
+
+- [x] ヘッダの変更
+- [x] トップページの変更
+- [x] コース一覧ページの変更
+- [ ] ポートフォリオページの変更
+- [ ] 未提出課題ページの変更
+- [ ] コースページの変更
+- [ ] リマインダページの変更
---
+++ src/content.js
@@ -0,0 +1,25 @@
+window.onload = () => {
+    let mynaviButtonHome = document.getElementsByClassName("mynavi-button-home")[0];
+    mynaviButtonHome.innerHTML = '<a href="home" class="mynavi-button-a">マイページ</a>';
+
+    let mynaviButtonCourse = document.getElementsByClassName("mynavi-button-course")[0];
+    mynaviButtonCourse.innerHTML = '<a href="home_course" class="mynavi-button-a">コース</a>';
+
+    let mynaviButtonPortfolio = document.getElementsByClassName("mynavi-button-portfolio")[0];
+    mynaviButtonPortfolio.innerHTML = '<a href="home_coursetable" class="mynavi-button-a">ポートフォリオ</a>';
+
+    let mynavi = document.getElementById("mynavi")
+    mynavi.innerHTML += '' +
+        '<div class="mynavi-button-home">' +
+            '<a href="home_library_query" class="mynavi-button-a">未提出課題</a>' +
+        '</div>' +
+        '<div class="mynavi-button-home">' +
+            '<a href="home_library_reminder" class="mynavi-button-a">リマインダ</a>' +
+        '</div>';
+
+    let mylangJa = document.getElementsByClassName("mylang-ja")[0]
+    mylangJa.innerHTML = '<a href="home_lang_en" class="mynavi-button-a">English</a>';
+
+    document.getElementsByClassName("align")[0].remove();
+    document.getElementsByClassName("pageheader-course")[0].remove();
+}
